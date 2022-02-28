052bfde18b61c9e35a782a67a750a12583a50bc3
Morhatok <100582581+morhatok@users.noreply.github.com>
2022-02-28 17:26:34 +0300

    update render.js
    new code

--- render.js
+++ render.js
@@ -1,2 +1,7 @@
 var productsContainer = document.querySelector('#products-container');
 console.log(productsContainer);
+async function getProducts() {
+	const response = await fetch ('.products.json');
+	console.log(response);
+}
+getProducts();
